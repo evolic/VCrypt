7615a338f69ed91ef821cd38a92ebd778ff31077
Tomasz Kuter <evolic@tomaszkuter.com>
2016-02-13 02:39:55 +0100

    [FIX][ZF2] Switched project to PSR-4 Loader, removed dependency from Zend\Loader\StandardAutoloader [FIX][composer][FirePHP] Restored FirePHP library in the composer.json

--- tests/_autoload.php
+++ tests/_autoload.php
@@ -4,12 +4,3 @@
  */
 
 include_once __DIR__ . '/../vendor/autoload.php';
-
-$loader = new Zend\Loader\StandardAutoloader(
-    array(
-        Zend\Loader\StandardAutoloader::LOAD_NS => array(
-            'VCryptTest' => __DIR__ . '/VCryptTest',
-        ),
-    )
-);
-$loader->register();
